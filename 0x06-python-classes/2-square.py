c4c31b7e8ab184870f4b52073852af5819b49a80
Sheila20 <shilanyambura47@gmail.com>
2022-09-14 05:56:31 -0700

    f

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""defines a class Square"""
+"""
+This module defines a Square class
+
+
+Its implements value and type checks for its attributes
+"""
+
+
 class Square:
-    """Represents a square
-    Attributes:
-        __size (int): size of a side of the square
+    """Square implementation
     """
     def __init__(self, size=0):
-        """initializes the square
-        Args:
-            size (int): size of a side of the square
-        Returns:
-            None
-        """
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        else:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = size 
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
-"""docstring for Square"""
+"""
+This module defines a Square class
+
+
+Its implements value and type checks for its attributes
+"""
+
 
 class Square:
-    """docstring for init"""
+    """Square implementation
+    """
     def __init__(self, size=0):
-        if isinstance(size, int) and size >= 0:
-            self.__size = size
-        elif not isinstance(size, int):
-            raise TypeError("size must be an integer")
+        if type(size) != int:
+            raise TypeError('size must be an integer')
         elif size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
     def area(self):
         return (self.__size ** 2)
