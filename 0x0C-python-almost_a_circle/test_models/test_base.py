0b4a3124128a7d678dfb63439b1cefb031a78324
Sheila20 <shilanyambura47@gmail.com>
2022-10-03 10:11:23 -0700

    d

--- 0x0C-python-almost_a_circle/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/test_models/test_base.py
@@ -1,31 +1,122 @@
 #!/usr/bin/python3
 """
-    Unittest for Base class
+A module that test differents behaviors
+of the Base class
 """
 import unittest
+import pep8
+import os
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square 
 
 
+class TestBase(unittest.TestCase):
+    """
+    A class to test the Base Class
+    """
+    def test_pep8_base(self):
+        """
+        Test that checks PEP8
+        """
+        syntax = pep8.StyleGuide(quit=True)
+        check = syntax.check_files(['models/base.py'])
+        self.assertEqual(
+            check.total_errors, 0,
+            "Found code style errors (and warnings)."
+        )
 
-class TestBaseclass(unittest.TestCase):
-    """ Test cases Base class"""
-    def setUp(self):
-        """ test raise heigt"""
-        self.inst = Base(5)
+    def test_id_as_positive(self):
+        ""r
+        Test for a positive Base Class id
+        """
+        base_instance = Base(115)
+        self.assertEqual(base_instance.id, 115)
+        base_instance = Base(67)
+        self.assertEqual(base_instance.id, 67)
 
-    def tearDown(self):
-        """ test raise heigt"""
-        Base._Base__nb_objects = 0
+    def test_id_as_negative(self):
+        """
+        Test for a negative Base Class id
+        """
+        base_instance = Base(-91)
+        self.assertEqual(base_instance.id, -91)
+        base_instance = Base(-4)
+        self.assertEqual(base_instance.id, -4)
 
-    def test_nb_objs_yes(self):
-        """ test raise heigt"""
-        self.assertEqual(self.inst._Base__nb_objects, 0)
+    def test_id_as_none(self):
+        """
+        Test for a None Base Class id
+        """
+        base_instance = Base()
+        self.assertEqual(base_instance.id, 1)
+        base_instance = Base(None)
+        self.assertEqual(base_instance.id, 2)
 
-    def test_nb_objs_no(self):
-        """ test raise heigt"""
-        self.inst = Base()
-        self.assertEqual(self.inst._Base__nb_objects, 1)
+    def test_string_id(self):
+        base_instance = Base('Monty Python')
+        self.assertEqual(base_instance.id, 'Monty Python')
+        base_instance = Base('Python is cool')
+        self.assertEqual(base_instance.id, 'Python is cool')
 
-    def test_id(self):
-        """ test raise heigt"""
-        self.assertEqual(self.inst.id, 5)
+    def test_to_json_string(self):
+        """
+        Test the to_json_string method
+        """
+        rect_instance = Rectangle(10, 7, 2, 8, 70)
+        rect_data = re1.to_dictionary()
+        json_data = Base.to_json_string([rect_data])
+        self.assertEqual(type(json_data), str)
+
+    def test_empty_to_json_string(self):
+        """
+        Test for a empty data on the to_json_string method
+        """
+        empty_data = []
+        json_data = Base.to_json_string(empty_data)
+        self.assertEqual(json_data, "[]")
+
+        empty_data = None
+        json_data = Base.to_json_string(empty_data)
+        self.assertEqual(json_data, "[]")
+
+    def test_instance(self):
+        """
+        Test a Base Class instance
+        """
+        base_instance = Base()
+        self.assertEqual(type(base_instance), Base)
+        self.assertTrue(isinstance(base_instance, Base))
+
+    def test_to_json_string(self):
+        """
+        Test a normal to_json_string functionality
+        """
+        rect_data = {'id': 31, 'x': 14, 'y': 11, 'width': 3, 'height': 3}
+        json_data = Base.to_json_string([rect_data])
+
+        self.assertTrue(isinstance(rect_data, dict))
+        self.assertTrue(isinstance(json_data, str))
+        self.assertCountEqual(
+            json_data,
+            '{["id": 31, "x": 14, "y": 11, "width": 3, "height": 3]}'
+        )
+
+    def test_wrong_to_json_string(self):
+        """
+        Test a wrong functionality of the
+        to_json_string method
+        """
+        json_data = Base.to_json_string(None)
+        self.assertEqual(json_data, "[]")
+
+        warn = ("to_json_string() missing 1 required positional argument: " +
+                "'list_dictionaries'")
+
+        with self.assertRaises(TypeError) as msg:
+            Base.to_json_string()
+
+        self.assertEqual(warn, str(msg.exception))
+
+        warn = "to_json_string() takes 1 positional argument but 2 were given"
+        
